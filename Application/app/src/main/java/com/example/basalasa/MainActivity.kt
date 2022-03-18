8037b8faa3bfde9d8a511c34563cc3361a742cdf
Jason Hoang <jasonhoang6201@gmail.com>
2022-03-18 20:11:29 +0700

    bottom navigation

--- Application/app/src/main/java/com/example/basalasa/MainActivity.kt
+++ Application/app/src/main/java/com/example/basalasa/MainActivity.kt
@@ -4,10 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    var bottomNav: BottomNavigationView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNav = findViewById(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragment)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment, R.id.categoryFragment, R.id.settingFragment, R.id.userFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        bottomNav!!.setupWithNavController(navController)
     }
 }
