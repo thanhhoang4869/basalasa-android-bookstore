db091e57839cd5e3eeba29f4c471eb3ae20e2791
lethanhkhoi <lethanhkhoi081001@gmail.com>
2022-03-31 12:49:27 +0700

    Update Book Detail

---
+++ Application/app/src/main/java/com/example/basalasa/activity/ReviewBooks.kt
@@ -0,0 +1,12 @@
+package com.example.basalasa.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.basalasa.R
+
+class ReviewBooks : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_review_books)
+    }
+}
